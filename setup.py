5b04cf3986939523db92e85bb196ea26cc8cfae3
Alex Vyushkov <baguage@gmail.com>
2013-09-05 22:45:45 -0400

    Initial version

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "django-auth-pubtkt",
+    version = "0.0.8",
+    author = "Alexander Vyushkov",
+    author_email = "alex.vyushkov@gmail.com",
+    description = ("Implementation of mod_auth_pubtkt as Django middleware"),
+    license = "BSD",
+    keywords = "django auth mod_auth_pubtkt",
+    url = "http://packages.python.org/django-auth-pubtkt",
+    packages=['django_auth_pubtkt', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+__author__ = 'Alexander Vyushkov'
+#__version__ = ..django_auth_pubtkt.__version__
